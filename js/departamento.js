cf1233d205e450b2793ab6e05932e8de89f8542e
Lider de desarrollo <liderdesarrollo@team.devzeros.com>
2020-12-04 15:05:14 -0500

    .

--- js/departamento.js
+++ js/departamento.js
@@ -1,6 +1,5 @@
 var colors = ['#007bff','#28a745','#333333','#c3e6cb','#dc3545','#6c757d'];
 
-
 var monedaDiv = $("#monedaDiv");
 var crecimientoDiv = $("#crecimientoDiv");
 var marcadorDiv =$("#marcadorDiv");
@@ -10,14 +9,31 @@ var graficoDiv = $("#graficoDiv")
 $(document).ready(function (){
 
 
+
     monedaDiv.load(monedaDiv.attr("src"));
     crecimientoDiv.load(crecimientoDiv.attr("src"));
     marcadorDiv.load(marcadorDiv.attr("src"));
     equipoDiv.load(equipoDiv.attr("src"));
     graficoDiv.load(graficoDiv.attr("src"));
 
+
+
+
+
     //loader Map Colombia
     $("#colombia").load("img/Colombia.svg");
+
+
+
+    $("Cesar").css("fill", "#e90000");
+   
+
+    //datos variables
+    var anio="2019";
+    document.getElementById("spanY").innerHTML = anio.toString();
+
+
+
     var items = document.querySelectorAll('.circle-menu-box a.menu-item');
 
     for(var i = 0, l = items.length; i < l; i++) {
@@ -26,26 +42,30 @@ $(document).ready(function (){
         items[i].style.top = (40 + 35*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
     }
 
+    setTimeout(function (){
 
 
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        var name = urlParams.get("name");
+        var idDpta = urlParams.get("id");
+        $("#"+idDpta).css("fill","#960303");
 
-
-    setTimeout(function(){
+        $("#departsv").load("img/departamentos/"+name+".svg");
 
 
         $("path.cls-1").on("mouseover",function(){
             $("path.cls-1").css("fill", "#d2d2e6");
             var randomColor = Math.floor(Math.random()*16777215).toString(16);
             $(this).css("fill", "#"+randomColor);
+            $("#"+idDpta).css("fill","#960303");
         })
 
         $("path.cls-1").click(function(){
             var url = $(this).attr("url");
             $("#colombia").css("width","90%");
             console.log("img/departamentos/"+$(this).attr("url"));
-            window.location.href = "departamento.html?name="+$(this).attr("url")+"&id="+$(this).attr("id")
-            return;
-            //$("#colombia").load("img/departamentos/"+$(this).attr("url")+".svg");
+            $("#colombia").load("img/departamentos/"+$(this).attr("url")+".svg");
         });
 
 
@@ -61,8 +81,8 @@ $(document).ready(function (){
 
         $(document).on('mousemove', function(e){
             $description.css({
-                left:  e.pageX-500,
-                top:   e.pageY - 300,
+                left:  e.pageX-250,
+                top:   e.pageY - 100,
                 height : "60px"
             });
 
@@ -79,14 +99,14 @@ $(document).ready(function (){
         $tableTooltip = $(".table-tooltip");
 
         $(".ee a").hover(function (){
-           var id = $(this).attr("data-id");
+            var id = $(this).attr("data-id");
             $(this).find("b").css("color","#960303")
-           if(id == "moneda"){
-               $("svg.icons#moneda  > g path").css("fill","#960303");
-           }else{
+            if(id == "moneda"){
+                $("svg.icons#moneda  > g path").css("fill","#960303");
+            }else{
 
-               $("svg.icons#"+id+">.st0").css("fill","#960303");
-           }
+                $("svg.icons#"+id+">.st0").css("fill","#960303");
+            }
             $("#"+id+".table-tooltip").attr("class", "cls-1 heyo");
             $tableTooltip.addClass('active');
             getHtmlTable(id,$tableTooltip)
@@ -101,15 +121,11 @@ $(document).ready(function (){
         });
 
 
+        function getHtmlTable(id, selector){
+            selector.html($("#"+id+"-table-table-tooltip").html());
+        }
+    },1000)
 
-
-    },1000);
-
-
-
-
-    function getHtmlTable(id, selector){
-        selector.html($("#"+id+"-table-table-tooltip").html());
-    }
 
 });
+
